ff1cb0d416548bb7a5d30d3aa45e464bf84afb8a
Davi Alves <flyingkrai@hotmail.com>
2013-11-22 00:43:31 -0300

    images, listing and css

--- assets/www/js/rest-services.js
+++ assets/www/js/rest-services.js
@@ -3,7 +3,7 @@
 angular.module('myApp.restServices', ['ngResource'])
 // EMPLOYEE RESOURCE
 .factory('Employee', ['$resource',
-  function ($resource) {
-    return $resource('http://localhost:3000/employees/:id', {});
+  function($resource) {
+		return $resource('http://localhost:3000/employees/:emplyeeId', {});
   }
 ]);;
